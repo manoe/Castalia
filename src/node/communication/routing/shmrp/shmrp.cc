3885b8c2be5b677c4710f85cef35331b98b2a382
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2022-10-13 16:25:02 +0200

    Seed code
     Changes to be committed:
    	modified:   node/communication/routing/shmrp/shmrp.cc
    	modified:   node/communication/routing/shmrp/shmrp.h
    	modified:   node/communication/routing/shmrp/shmrp.msg

--- src/node/communication/routing/shmrp/shmrp.cc
+++ src/node/communication/routing/shmrp/shmrp.cc
@@ -13,25 +13,144 @@ Define_Module(shmrp);
 
 void shmrp::startup() {
     cModule *appModule = getParentModule()->getParentModule()->getSubmodule("Application");
-    if(isSink()) 
+    if(appModule->hasPar("isSink")) {
+        g_is_sink=appModule->par("isSink");
+    } else {
+        g_is_sink=false;
+    }
+
+    if(isSink()) {
+        setHop(0);
+        setRound(1);
+        setTimer(shmrpTimerDef::SINK_START,par("t_start"));
+    } else {
+        setHop(std::numeric_limits<int>::max());
+        setRound(0);
+    }
 }
+
+bool shmrp::isSink() const {
+    return g_is_sink;
+}
+
+void shmrp::setSinkAddress(const char *p_sink_addr) {
+    g_sink_addr.assign(p_sink_addr);
+}
+
+std::string shmrp::getSinkAddress() const {
+    return g_sink_addr;
+}
+
+void shmrp::setHop(int hop) {
+    g_hop=hop;
+}
+
+int shmrp::getHop() const {
+    return g_hop;
+}
+
+void shmrp::setRound(int round) {
+    g_round=round;
+}
+
+int shmrp::getRound() const {
+    return g_round;
+}
+
+void shmrp::setState(shmrpStateDef state) {
+    g_state=state;
+}
+
+shmrpStateDef shmrp::getState() const {
+    return g_state;
+}
+
+
+void shmrp::sendRinv(int round, int pathid) {
+    trace()<<"[info] Entering shmrp::sendRinv(round = "<<round<<", pathid = "<<pathid<<")";
+    shmrpRinvPacket *rinv_pkt=new shmrpRinvPacket("SHMRP RINV packet", NETWORK_LAYER_PACKET);
+    rinv_pkt->setShmrpPacketKind(shmrpPacketDef::RINV_PACKET);
+    rinv_pkt->setSource(SELF_NETWORK_ADDRESS);
+    rinv_pkt->setDestination(BROADCAST_NETWORK_ADDRESS);
+    rinv_pkt->setRound(round);
+    rinv_pkt->setPathid(0);
+    rinv_pkt->setHop(getHop());
+    rinv_pkt->setSequenceNumber(currentSequenceNumber++);
+    toMacLayer(rinv_pkt, BROADCAST_MAC_ADDRESS);
+}
+
+void shmrp::sendRinv(int round) {
+    trace()<<"[info] Entering shmrp::sendRinv(round = "<<round<<")";
+    sendRinv(round,0);
+}
+
 
 void shmrp::timerFiredCallback(int index) {
     switch (index) {
+        case shmrpTimerDef::SINK_START: {
+            trace()<<"[timer] SINK_START timer expired";
+            sendRinv(getRound());
+            setRound(1+getRound());
+            break;
+        }
         default: {
+            trace()<<"[error] Unknown timer expired: "<<index;
+            break;
         }
     }
 }
 
 void shmrp::fromApplicationLayer(cPacket * pkt, const char *destination) {
-    if(0!=std::strcmp(destination,g_sink_addr)) {
+    if(0!=std::strcmp(destination,getSinkAddress().c_str())) {
         trace()<<"[error] Packet's destination not sink: "<<destination;
         return;
     }
 }
 
 void shmrp::fromMacLayer(cPacket * pkt, int srcMacAddress, double rssi, double lqi) {
-    return;
+    shmrpPacket *net_pkt=dynamic_cast<shmrpPacket *>(pkt);
+    if(!net_pkt) {
+        trace()<<"[error] Dynamic cast of packet failed";
+    }
+
+    switch (net_pkt->getShmrpPacketKind()) {
+        case shmrpPacketDef::RINV_PACKET: {
+            trace()<<"[info] RINV_PACKET received";
+            auto rinv_pkt=dynamic_cast<shmrpRinvPacket *>(pkt);
+
+            if(isSink()) {
+                trace()<<"[info] RINV_PACKET rejected by Sink";
+                break;
+            }
+
+            if(rinv_pkt->getRound() > getRound()) {
+                setState(shmrpStateDef::LEARN);
+                /* start learning process */
+            }
+
+            break;
+        }
+        case shmrpPacketDef::RREQ_PACKET: {
+            trace()<<"[info] RREQ_PACKET received";
+            break;
+        }
+        case shmrpPacketDef::RRESP_PACKET: {
+            trace()<<"[info] RRESP_PACKET received";
+            break;
+        }
+        case shmrpPacketDef::DATA_PACKET: {
+            trace()<<"[info] DATA_PACKET received";
+            break;
+        }
+        case shmrpPacketDef::UNDEF_PACKET: {
+            trace()<<"[error] UNDEF_PACKET received";
+            break;
+        }
+        default: {
+            trace()<<"[error] Unknown packet received with shmrpPacketKind value: "<<net_pkt->getShmrpPacketKind();
+            break;
+        }
+    }
 }
 
 void shmrp::finishSpecific() {
