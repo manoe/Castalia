2ad52c6683b25738b0a2993bab0f40222fe7c3d4
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2023-05-18 13:27:40 +0200

    Shmrp node function test
     Changes to be committed:
    	new file:   batch.sh
    	new file:   gen.sh
    	new file:   omnetpp.ini

---
+++ Simulations/shmrp_nf/batch.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -x
+
+REP=1
+
+#SEQ=`seq 11 5 31`
+
+SEQ="0.8"
+
+for j in $SEQ
+do
+yq --null-input '{"runs": []}' > shmrp_meas_qos_pdr_`echo $j|sed 's/\./_/g'`.yaml
+done
+
+# sed 's/\./_/g'
+
+
+for i in $(seq 1 $REP)
+do
+     SEED=1678184087
+     for j in $SEQ
+	do
+		./gen.sh omnetpp.ini qos_pdr=$j $SEED shmrp_meas_qos_pdr_`echo $j|sed 's/\./_/g'`.yaml
+	done
+done
