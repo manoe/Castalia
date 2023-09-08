7b66002617a906e587c1fbc1e2a2712cbf5ff60d
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2023-09-08 09:05:59 +0200

    EFMRP seed code
     Changes to be committed:
    	new file:   efmrp/efmrp.cc
    	new file:   efmrp/efmrp.h
    	new file:   efmrp/efmrp.msg

---
+++ src/node/communication/routing/efmrp/efmrp.h
@@ -0,0 +1,102 @@
+/*******************************************************************************
+ *  Copyright: Balint Aron Uveges, 2023                                        *
+ *  Developed at Pazmany Peter Catholic University,                            *
+ *               Faculty of Information Technology and Bionics                 *
+ *  Author(s): Balint Aron Uveges                                              *
+ *  This file is distributed under the terms in the attached LICENSE file.     *
+ *                                                                             *
+ *******************************************************************************/
+
+#ifndef _EFMRP_H_
+#define _EFMRP_H_
+
+#include <random>                                              
+#include <algorithm>                                           
+#include <map>                                                 
+#include <fstream>                                             
+#include <unordered_set>                                       
+#include <ctime>                                               
+#include <cstring>                                             
+#include <yaml-cpp/yaml.h>                                     
+ 
+#include "node/communication/routing/VirtualRouting.h"
+#include "node/communication/routing/efmrp/efmrp_m.h"
+#include "node/application/ApplicationPacket_m.h"
+#include "node/application/ForestFire/forest_fire_packet_m.h"
+#include "node/application/ForestFire/forest_fire.h"
+#include "node/mobilityManager/VirtualMobilityManager.h"
+
+enum efmrpStateDef {
+    UNDEF   = 0,
+    INIT    = 1,
+    LEARN   = 2,
+    BUILD   = 3,
+    WORK    = 4
+};
+
+enum efmrpTimerDef {
+    SINK_START  = 1,
+    TTL         = 2  
+};
+
+struct node_entry {
+    string nw_address;
+    int    hop;
+    double nrg;
+    double env;
+};
+
+struct feat_par {
+    double ttl;
+};
+
+
+class efmrp: public VirtualRouting {
+    private:
+        bool            g_is_sink;
+        string          g_sink_addr;
+        int             g_hop;
+        feat_par        fp;
+        efmrpStateDef   g_state;
+
+        std::map<std::string,node_entry> hello_table;
+        std::map<std::string,node_entry> field_table;
+        std::map<std::string,node_entry> routing_table;
+
+        
+        ForestFire* ff_app;
+
+        YAML::Emitter y_out;
+
+    protected:
+        void startup();
+        void fromApplicationLayer(cPacket *, const char *);
+        void fromMacLayer(cPacket *, int, double, double);
+        void timerFiredCallback(int);
+//        void finishSpecific();
+
+        void sendHello();
+        void sendHello(int, double);
+        void updateHelloTable(efmrpHelloPacket *);
+
+        void sendField(int, double, double);
+        void updateFieldTable(efmrpFieldPacket *);
+
+        bool isSink() const;
+        void setSinkAddress(const char *);
+        std::string getSinkAddress() const;
+
+        void setHop(int);
+        int getHop() const;
+
+        void setState(efmrpStateDef);
+        std::string stateToStr(efmrpStateDef) const;
+        efmrpStateDef getState() const;
+
+    public:
+        efmrp() : g_is_sink(false),
+                  g_hop(std::numeric_limits<int>::max()),
+                  g_state(efmrpStateDef::UNDEF) {};
+};
+
+#endif /* _EFMRP_H_ */
