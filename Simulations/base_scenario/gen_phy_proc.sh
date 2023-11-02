746fe72b1729f73b5d677667ddeb278312dcba03
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2023-11-02 14:42:46 +0100

    Base scenario extended

---
+++ Simulations/base_scenario/gen_phy_proc.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+case $1 in
+
+  "none")
+    touch physical_process.ini
+    ;;
+
+  "northeast")
+    echo "include phy_proc_northeast.ini" > physical_process.ini
+    ;;
+
+  "center")
+    echo "include phy_proc_center.ini" > physical_process.ini
+    ;;
+
+  *)
+    echo "unknown placement"
+    ;;
+esac
+
