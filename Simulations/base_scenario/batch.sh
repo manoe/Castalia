67b252fcf23d4497f5471e590f3084b6ad95de69
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2023-11-02 22:20:47 +0100

    New parameter: nullEnergyOnFail - zero remainingEnergy if node fails

--- Simulations/base_scenario/batch.sh
+++ Simulations/base_scenario/batch.sh
@@ -4,15 +4,9 @@ PROTOS="efmrp shmrp flooding hdmrp"
 
 for i in $PROTOS
 do
-    yq --null-input '{"runs": []}' > ${i}.yaml
-    yq --null-input '{"runs": []}' > ${i}_pkt.yaml
-done
-
-for i in $PROTOS
-do
     SEED=1678184087
     ./gen_routing.sh $i
     ./gen.sh omnetpp.ini qos_pdr=0.8 $SEED ${i}.yaml
-    yq -i '.runs += [ load("pkt.yaml")]' ${i}_pkt.yaml
+    mv nrg.yaml ${i}_nrg.yaml
 done
 
