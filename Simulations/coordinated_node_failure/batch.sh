58518e96765d70a249f4d8ecdd6407ff74f843a6
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2023-11-13 21:13:31 +0100

    Sync everything

--- Simulations/coordinated_node_failure/batch.sh
+++ Simulations/coordinated_node_failure/batch.sh
@@ -1,38 +1,48 @@
 #!/bin/sh -x
 
-PROTO="shmrp efmrp hdmrp"
-FAIL=3
+#PROTO="shmrp efmrp hdmrp"
+PROTO="shmrp"
+FAIL=0
+ITER=2
 NUM=`expr $FAIL + 1`
 
-SEED=1678184087
+SEED=`date +%s`
+SEED_SET=`./rand.py -i $ITER $SEED`
 
 for p in $PROTO
 do
-    rm loc_pdr.yaml
-    rm base_loc_pdr.yaml
-    rm fail.ini
-    touch fail.ini
-
-    for i in `seq $NUM`
+    yq --null-input '{"runs": []}' > ${p}_pkt.yaml
+    yq --null-input '{"runs": []}' > ${p}_nrg.yaml
+    yq --null-input '{"runs": []}' > ${p}_pdr.yaml
+    for r in $SEED
     do
-        if [ -f Castalia-Trace.txt ]
-        then
-            rm Castalia-Trace.txt
-        fi
-        ./gen_routing.sh $p
-        ./gen_sim.sh `expr 1800 \* ${i}`
-        ./gen.sh omnetpp.ini qos_pdr=0.8 $SEED omnetpp.yaml
-        if [ $FAIL -ge $i ]
-        then
-            if [ -f base_loc_pdr.yaml ]
-            then
-                ./add_fail.sh `~/crunchr/failr/main.py -f loc_pdr.yaml -b base_loc_pdr.yaml` `expr 1800 \* ${i}`
-            else
-                ./add_fail.sh `~/crunchr/failr/main.py -f loc_pdr.yaml` `expr 1800 \* ${i}`
-            fi
-        mv loc_pdr.yaml base_loc_pdr.yaml
-        fi
-    done
+        rm loc_pdr.yaml
+        rm base_loc_pdr.yaml
+        rm fail.ini
+        touch fail.ini
     
-    mv nrg.yaml ${p}_nrg.yaml
+        for i in `seq $NUM`
+        do
+            if [ -f Castalia-Trace.txt ]
+            then
+                rm Castalia-Trace.txt
+            fi
+            ./gen_routing.sh $p
+            ./gen_sim.sh `expr 1800 \* ${i}`
+            ./gen.sh omnetpp.ini qos_pdr=0.8 $r ${p}_pdr.yaml
+            if [ $FAIL -ge $i ]
+            then
+                if [ -f base_loc_pdr.yaml ]
+                then
+                    ./add_fail.sh `~/crunchr/failr/main.py -f loc_pdr.yaml -b base_loc_pdr.yaml` `expr 1800 \* ${i}`
+                else
+                    ./add_fail.sh `~/crunchr/failr/main.py -f loc_pdr.yaml` `expr 1800 \* ${i}`
+                fi
+            mv loc_pdr.yaml base_loc_pdr.yaml
+            fi
+        done
+        mv fail.ini ${p}_${r}_fail.ini
+        yq -i '.runs += [ load("pkt.yaml")]' ${p}_pkt.yaml
+        yq -i '.runs += [ load("nrg.yaml")]' ${p}_nrg.yaml
+    done
 done
