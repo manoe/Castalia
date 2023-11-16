cb25452e78085d3f86da3edd2f5f85f08e2f91b9
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2023-11-16 21:53:49 +0100

    More fixes

--- Simulations/shmrp_master_ratio/gen_master.sh
+++ Simulations/shmrp_master_ratio/gen_master.sh
@@ -15,4 +15,6 @@ do
     echo "SN.node[${n}].Application.isMaster = true" >> master.ini
     echo "SN.node[${n}].ResourceManager.isMaster = true" >> no_depletion.ini
 done
-    
+
+echo "SN.node[0].Application.isMaster = true" >> master.ini           
+echo "SN.node[0].ResourceManager.isMaster = true" >> no_depletion.ini
