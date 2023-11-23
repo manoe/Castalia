6c25016dea0b21ed97777167ca88bf734942c849
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2023-11-23 16:24:32 +0100

    Update

--- Simulations/rectangle_ff_event/batch.sh
+++ Simulations/rectangle_ff_event/batch.sh
@@ -1,18 +1,24 @@
 #!/bin/sh -x
 
 PROTOS="efmrp shmrp hdmrp flooding"
-
+ITER=10
 SEED=`date +%s`
 SEED_SET=`./rand.py -i $ITER $SEED`
 echo $SEED_SET > seed_set.txt
+
+for p in $PROTOS
+do
+    yq --null-input '{"runs": []}' > ${p}_pdr.yaml
+done
 
 ./gen_phy_proc.sh rectangular
 
 for s in $SEED_SET
 do
-    for i in $PROTOS
+    for p in $PROTOS
     do
-    ./gen_routing.sh $i
-    ./gen.sh omnetpp.ini qos_pdr=0.6 $SEED ${i}.yaml
+        ./gen_routing.sh $p
+        ./gen.sh omnetpp.ini qos_pdr=0.6 ${s} ${p}_pdr.yaml
+    done
 done
 
