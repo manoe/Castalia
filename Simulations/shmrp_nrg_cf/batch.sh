8195b7c86a023835968506e78e43128a9a3e745c
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2023-11-27 19:55:05 +0100

    Tune energy component of CF

---
+++ Simulations/shmrp_nrg_cf/batch.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -x
+
+#PROTOS="efmrp shmrp hdmrp flooding"
+#PROTOS="hdmrp"
+PROTOS=efmrp
+
+for i in $PROTOS
+do
+    SEED=1678184087
+    ./gen_routing.sh $i
+    ./gen.sh omnetpp.ini qos_pdr=0.8 $SEED ${i}.yaml
+    mv nrg.yaml ${i}_nrg.yaml
+    mv pdr.yaml ${i}_pdr.yaml
+    mv pkt.yaml ${i}_pkt.yaml
+done
+
