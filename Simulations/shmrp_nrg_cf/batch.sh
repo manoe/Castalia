8771b1351f4ec13ef6019b6239dd2a867d98c642
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2023-11-27 20:37:54 +0100

    Config update

--- Simulations/shmrp_nrg_cf/batch.sh
+++ Simulations/shmrp_nrg_cf/batch.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -x
 
-#PROTOS="efmrp shmrp hdmrp flooding"
-#PROTOS="hdmrp"
-PROTOS=efmrp
+PROTOS=shmrp
+
+./gen_phy_proc.sh none
 
 for i in $PROTOS
 do
     SEED=1678184087
     ./gen_routing.sh $i
-    ./gen.sh omnetpp.ini qos_pdr=0.8 $SEED ${i}.yaml
+    ./gen.sh omnetpp.ini qos_pdr=0.6 $SEED ${i}.yaml
     mv nrg.yaml ${i}_nrg.yaml
     mv pdr.yaml ${i}_pdr.yaml
     mv pkt.yaml ${i}_pkt.yaml
