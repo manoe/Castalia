f643e09fdd631228ffb64cb9dae4712fada93065
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2023-11-27 21:43:25 +0100

    Finishing touches

--- Simulations/shmrp_nrg_cf/batch.sh
+++ Simulations/shmrp_nrg_cf/batch.sh
@@ -1,16 +1,22 @@
 #!/bin/sh -x
 
-PROTOS=shmrp
+PROTO=shmrp
+ITER=10
+SEED=`date +%s`
+SEED_SET=`python3 ./rand.py -i $ITER $SEED`
+echo $SEED_SET > seed_set.txt
+NRG_ETA="0.0 0.1 0.2 0.3 0.4 0.5"
 
 ./gen_phy_proc.sh none
+./gen_routing.sh ${PROTO}
+yq --null-input '{"runs": []}' > ${PROTO}_pdr.yaml
 
-for i in $PROTOS
+for s in ${SEED_SET}
 do
-    SEED=1678184087
-    ./gen_routing.sh $i
-    ./gen.sh omnetpp.ini qos_pdr=0.6 $SEED ${i}.yaml
-    mv nrg.yaml ${i}_nrg.yaml
-    mv pdr.yaml ${i}_pdr.yaml
-    mv pkt.yaml ${i}_pkt.yaml
+    for n in ${NRG_ETA}
+    do
+        ./gen.sh omnetpp.ini qos_pdr=0.6,eta_nrg=${n} $SEED ${PROTO}_pdr.yaml
+        exit
+    done
 done
 
