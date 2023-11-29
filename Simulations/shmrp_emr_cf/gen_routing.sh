2affdb1e9fd67bff6e7f52a69d4354467f9d7374
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2023-11-29 21:45:17 +0100

    Environment

---
+++ Simulations/shmrp_emr_cf/gen_routing.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+case $1 in
+
+  "shmrp")
+    echo "include shmrp.ini" > routing.ini
+    ;;
+
+  "efmrp")
+    echo "include efmrp.ini" > routing.ini
+    ;;
+
+  "aodv")
+    echo "include aodv.ini" > routing.ini
+    ;;
+
+  "hdmrp")
+    echo "include hdmrp.ini" > routing.ini
+    ;;
+  "flooding")
+    echo "include flooding.ini" > routing.ini
+    ;;
+
+  *)
+    echo "unknown protocol"
+    ;;
+esac
+
