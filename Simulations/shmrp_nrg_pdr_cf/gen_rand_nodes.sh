0d95ecd0caa1941feec4a427c4cf48d2e17b4dfa
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2023-11-30 20:30:41 +0100

    New uc

---
+++ Simulations/shmrp_nrg_pdr_cf/gen_rand_nodes.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+
+rm nrg.ini
+cp nrg_base.ini nrg.ini
+
+NODES=`python3 ./rand_sel.py -p -s ${2} -n ${1} -f ${3}`
+
+echo $NODES
+
+for n in $NODES
+do
+    echo "SN.node[${n}].ResourceManager.initialEnergy = 20" >> nrg.ini
+done
