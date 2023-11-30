cd42045323987f5b46241094b0368686a4aaccdb
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2023-11-30 16:47:08 +0100

    Repeat emergency

--- Simulations/shmrp_emr_cf/batch.sh
+++ Simulations/shmrp_emr_cf/batch.sh
@@ -5,7 +5,7 @@ ITER=10
 SEED=`date +%s`
 SEED_SET=`./rand.py -i $ITER $SEED`
 echo $SEED_SET > seed_set.txt
-EMR_EPSILON="0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.8 1.0"
+EMR_EPSILON="0.0 0.05 0.1 0.15 0.2"
 
 for e in ${EMR_EPSILON}
 do
