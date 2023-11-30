2aad0650563b9cea3df9b4f901ed7146aeb51a1d
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2023-11-30 20:45:09 +0100

    Update

--- Simulations/shmrp_nrg_cf/batch_rand_sel.sh
+++ Simulations/shmrp_nrg_cf/batch_rand_sel.sh
@@ -5,7 +5,7 @@ ITER=10
 SEED=`date +%s`
 SEED_SET=`python3 ./rand.py -i $ITER $SEED`
 echo $SEED_SET > seed_set.txt
-NRG_ETA="0.0 0.05 0.1 0.15 0.2"
+NRG_ETA="0.0 0.05 0.1"
 
 ./gen_phy_proc.sh none
 ./gen_routing.sh ${PROTO}
@@ -22,11 +22,11 @@ for s in ${SEED_SET}
 do
      rm nrg.ini
      cp nrg_base.ini nrg.ini
-    ./gen.sh omnetpp.ini qos_pdr=0.6,eta_nrg=0.0,sim_time=800s ${s} ${PROTO}_nrg_test_pdr.yaml
-    ./gen_rand_nodes.sh 4 ${s} "loc_pdr.yaml"
+    ./gen.sh omnetpp.ini qos_pdr=0.4,eta_nrg=0.0,sim_time=800s ${s} ${PROTO}_nrg_test_pdr.yaml
+    ./gen_rand_nodes.sh 6 ${s} "loc_pdr.yaml"
     for n in ${NRG_ETA}
     do
-        ./gen.sh omnetpp.ini qos_pdr=0.6,eta_nrg=${n} ${s} ${PROTO}_nrg_${n}_pdr.yaml
+        ./gen.sh omnetpp.ini qos_pdr=0.4,eta_nrg=${n} ${s} ${PROTO}_nrg_${n}_pdr.yaml
         yq -i '.runs += [ load("nrg.yaml")]' ${PROTO}_nrg_${n}_nrg.yaml
     done
 done
