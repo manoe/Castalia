af701a97a38eecd7574e53d8e4facd3cfa3de47f
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2023-12-06 20:59:52 +0100

    Update

--- Simulations/shmrp_ff_event/batch.sh
+++ Simulations/shmrp_ff_event/batch.sh
@@ -2,10 +2,17 @@
 
 PROTO=shmrp
 ITER=1
-SEED=`date +%s`
-SEED_SET=`python3 ./rand.py -i $ITER $SEED`
-echo $SEED_SET > seed_set.txt
-TIMES="3599"
+
+if [ "$1" ]
+then
+    SEED_SET=$1
+else
+    SEED=`date +%s`
+    SEED_SET=`python3 ./rand.py -i $ITER $SEED`
+    echo $SEED_SET > seed_set.txt
+fi
+
+TIMES="1799 5000"
 
 ./gen_phy_proc.sh custom
 ./gen_routing.sh ${PROTO}
@@ -14,14 +21,16 @@ for t in ${TIMES}
 do
     yq --null-input '{"runs": []}' > ${PROTO}_ff_time_${t}_pdr.yaml
     yq --null-input '{"runs": []}' > ${PROTO}_ff_time_${t}_nrg.yaml
+    yq --null-input '{"runs": []}' > ${PROTO}_ff_time_${t}_pkt.yaml
 done
 
 for s in ${SEED_SET}
 do
     for t in ${TIMES}
     do
-        ./gen.sh omnetpp.ini qos_pdr=0.6,sim_time=${t}s ${s} ${PROTO}_ff_time_${t}_pdr.yaml
+        ./gen.sh omnetpp.ini qos_pdr=0.6,sim_time=${t}s,side ${s} ${PROTO}_ff_time_${t}_pdr.yaml
         yq -i '.runs += [ load("nrg.yaml")]' ${PROTO}_ff_time_${t}_nrg.yaml
+        yq -i '.runs += [ load("pkt.yaml")]' ${PROTO}_ff_time_${t}_pkt.yaml
     done
 done
 
