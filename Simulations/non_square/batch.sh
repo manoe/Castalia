965c4b5b3e1189167db1614c386c9250dc27bb48
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2024-01-29 13:57:47 +0100

    Update lots of files, remove others.

--- Simulations/non_square/batch.sh
+++ Simulations/non_square/batch.sh
@@ -5,43 +5,22 @@ ITER=10
 SEED=`date +%s`
 SEED_SET=`python3 ./rand.py -i $ITER $SEED`
 echo $SEED_SET > seed_set.txt
-MAS_MU=`seq 0.0 0.1 0.3`
-PDR_PI=`seq 0.0 0.1 0.3`
-NODE_NUM=64
-M_RATIO=30
-
+X_NODE=4
+Y_NODE=16
 #
 # POSITION????
 #
 ./gen_phy_proc.sh none
 ./gen_routing.sh ${PROTO}
-touch master.ini
+./gen_deployment.sh ${X_NODE} ${Y_NODE}
 
-for n in ${MAS_MU}
-do
-   for p in ${PDR_PI}
-    do
-        yq --null-input '{"runs": []}' > ${PROTO}_mas_${n}_pdr_${p}_pdr.yaml
-        yq --null-input '{"runs": []}' > ${PROTO}_mas_${n}_pdr_${p}_nrg.yaml
-        yq --null-input '{"runs": []}' > ${PROTO}_mas_test_pdr.yaml
-    done
-done
+PREFIX="${PROTO}_${X_NODE}x${Y_NODE}"
+
+yq --null-input '{"runs": []}' > ${PREFIX}_pdr.yaml
+yq --null-input '{"runs": []}' > ${PREFIX}_nrg.yaml
 
 for s in ${SEED_SET}
 do
-     rm master.ini
-     touch master.ini
-    ./gen.sh omnetpp.ini qos_pdr=0.4,mu_mas=0.0,pi_pdr=0.0,sim_time=800s ${s} ${PROTO}_mas_test_pdr.yaml
-    ./gen_master.sh ${NODE_NUM} ${M_RATIO} ${s}
-    yq --null-input '{"runs": []}' > ${PROTO}_nrg_test_pdr.yaml
-
-    for n in ${MAS_MU}
-    do
-        for p in ${PDR_PI}
-        do
-            ./gen.sh omnetpp.ini qos_pdr=0.4,mu_mas=${n},pi_pdr=${p} ${s} ${PROTO}_mas_${n}_pdr_${p}_pdr.yaml
-            yq -i '.runs += [ load("nrg.yaml")]' ${PROTO}_mas_${n}_pdr_${p}_nrg.yaml
-        done
-    done
+    ./gen.sh omnetpp.ini non_square ${s} ${PREFIX}_pdr.yaml
+    yq -i '.runs += [ load("nrg.yaml")]' ${PREFIX}_nrg.yaml
 done
-
--- Simulations/non_square/gen_deployment.sh
+++ Simulations/non_square/gen_deployment.sh
@@ -2,7 +2,7 @@
 
 # $1 x $2 y
 
-export SIM_TIME=`echo 2h`
+export SIM_TIME=`echo 3600`
 export FIELD_X=`echo "22.8 * (${1} - 1)"|bc `
 export FIELD_Y=`echo "22.8 * (${2} - 1)"|bc `
 export NUM_NODES=`echo "${1} * ${2}"|bc `
