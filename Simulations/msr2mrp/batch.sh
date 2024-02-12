03112dc159226232396e5306f3a57d63946b0e0a
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2024-02-12 11:47:17 +0100

    Some tweaking

---
+++ Simulations/msr2mrp/batch.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -x
+
+PROTO=msr2mrp
+ITER=1
+SEED=`date +%s`
+SEED_SET=`python3 ./rand.py -i $ITER $SEED`
+#echo $SEED_SET > seed_set.txt
+
+./gen_phy_proc.sh none
+./gen_routing.sh ${PROTO}
+yq --null-input '{"runs": []}' > ${PROTO}_pdr.yaml
+
+for s in ${SEED_SET}
+do
+    ./gen.sh omnetpp.ini qos_pdr=0.6 ${s} ${PROTO}_pdr.yaml
+done
+
