7f6c73e4a5e16d8f80298a6309855ddcca1f2f99
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2024-03-10 13:25:41 +0100

    SMRP smoke test
     Changes to be committed:
    	new file:   aodv.ini
    	new file:   batch.sh
    	new file:   deployment.tmpl
    	new file:   efmrp.ini
    	new file:   flooding.ini
    	new file:   gen.sh
    	new file:   gen_deployment.sh
    	new file:   gen_phy_proc.sh
    	new file:   gen_routing.sh
    	new file:   hdmrp.ini
    	new file:   msr2mrp.ini
    	new file:   omnetpp.ini
    	new file:   phy_proc.yaml
    	new file:   phy_proc_center.ini
    	new file:   phy_proc_none.ini
    	new file:   phy_proc_northeast.ini
    	new file:   rand.py
    	new file:   rand_sel.py
    	new file:   shmrp.ini
    	new file:   smrp.ini

---
+++ Simulations/smrp_smoke_test/batch.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -x
+
+PROTO=smrp
+ITER=1
+#SEED=`date +%s`
+#SEED_SET=`python3 ./rand.py -i $ITER $SEED`
+SEED_SET=597428753
+#echo $SEED_SET > seed_set.txt
+#SCALE=`seq 1 0.1 2`
+SCALE=1
+
+X_NODE=8
+Y_NODE=8
+
+./gen_phy_proc.sh none
+./gen_routing.sh ${PROTO}
+
+for i in $SCALE
+do
+    ./gen_deployment.sh ${X_NODE} ${Y_NODE} `echo "${i} * 22.8"|bc`
+    yq --null-input '{"runs": []}' > ${PROTO}_map_scale_${i}_pdr.yaml
+    for s in ${SEED_SET}
+    do
+        ./gen.sh omnetpp.ini qos_pdr=0.6,non_square ${s} ${PROTO}_map_scale_${i}_pdr.yaml
+    done
+done
---
+++ Simulations/smrp_smoke_test/gen.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -x
+
+echo "config file:" $1
+CFG=$1
+echo "args: " $2
+ARG=$2
+echo "repeats:" $3
+SEED=$3
+echo "out file:" $4
+OUT=$4
+
+if [ "`python3 --version`" = "Python 3.6.3" ]
+then
+    ../../bin/Castalia -i $CFG -c seed=$SEED,$ARG
+else
+    python3 ../../bin/Castalia3 -d -i $CFG -c seed=$SEED,$ARG
+fi
+
+yq -i e '. += { "loc_pdr": load("loc_pdr.yaml") }' pdr.yaml
+yq -i '.runs += [ load("pdr.yaml")]' $OUT
+
