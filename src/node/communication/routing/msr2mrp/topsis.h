9ad13fbba96df38c65047fe322ab472baec2f7b6
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2024-11-27 11:51:34 +0100

    normalisation done

--- src/node/communication/routing/msr2mrp/topsis.h
+++ src/node/communication/routing/msr2mrp/topsis.h
@@ -21,13 +21,40 @@ struct ps_alt {
     int pathid;
 };
 
-
+struct ps_alt_r : public ps_alt {
+    double rank;
+};
 
 class TopsisEngine {
     private:
-        
+        mat table;
+        int alt_num;
+        std::vector<ps_alt> alts;
+    protected:
+        mat normalizeMatrix() {
+            mat pow_table = square(table);
+            mat norm_table = table;
+            rowvec norm(table.n_cols,fill::zeros);
+            for(int i=0; i < table.n_cols ; ++i) {
+                norm(i)=sum(pow_table.col(i));
+            }
+            norm=sqrt(norm);
+            for(int i=0; i < table.n_rows ; ++i) {
+                norm_table.row(i) / norm;
+            }
+            return norm_table;
+        };
     public:
-        TopsisEngine(int alt) {
+        TopsisEngine(int alt_num): alt_num(alt_num) {
+            table=mat(0, 4, fill::zeros);
+        };
+        void addAlternative(ps_alt id, int hop, double pdr, double nrg, double env) {
+            alts.push_back(id);
+            rowvec v({static_cast<double>(hop), pdr, nrg, env});
+            table.row(table.n_rows)=v;
+        };
+        std::vector<ps_alt_r> getRanking() {
+            auto n_table=normalizeMatrix();
 
         };
 };
