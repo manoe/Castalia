da0cc585f5e43e9f955d158909ca27d2f3cae9ad
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2024-11-27 10:09:50 +0100

    TOPSIS skeleton

---
+++ src/node/communication/routing/msr2mrp/topsis.h
@@ -0,0 +1,21 @@
+//*******************************************************************************
+//*  Copyright: Balint Aron Uveges, 2024                                        *
+//*  Developed at Pazmany Peter Catholic University,                            *
+//*               Faculty of Information Technology and Bionics                 *
+//*  Author(s): Balint Aron Uveges                                              *
+//*  This file is distributed under the terms in the attached LICENSE file.     *
+//*                                                                             *
+//*******************************************************************************
+
+
+#ifndef _TOPSIS_H_
+#define _TOPSIS_H_
+
+#include <cmath>
+
+struct ps_alt {
+    int sink;
+    int pathid;
+};
+
+#endif // _TOPSIS_H_
