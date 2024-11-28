1c4e527176494a0d2ddc29e24a6e37f3ee1a6896
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2024-11-28 11:08:09 +0100

    TOPSIS corrections

--- src/node/communication/routing/msr2mrp/msr2mrp.cc
+++ src/node/communication/routing/msr2mrp/msr2mrp.cc
@@ -2366,6 +2366,12 @@ void msr2mrp::fromApplicationLayer(cPacket * pkt, const char *destination) {
             case msr2mrpLbMechDef::TOPSIS: {
                 trace()<<"[info] TOPSIS-rank based selection";
                 auto rt = collectAllRoutes(ev);
+                trace()<<"[info] Available routes: "<<rt.size();
+                if(!rt.size()) {
+                    trace()<<"[error] No route available";
+                    delete pkt;
+                    break;
+                }
                 TopsisEngine te(rt.size());
                 for(auto re: rt) {
                     te.addAlternative({re.sink,re.pathid[0].pathid},re.hop,static_cast<double>(pkt_table[re.nw_address].ack_count)/static_cast<double>(pkt_table[re.nw_address].pkt_count), re.pathid[0].enrgy ,re.pathid[0].emerg);
