c37849dd271f71edee4b4afbac54246a36a991ff
Balint Uveges <uveges.balint.aron@itk.ppke.hu>
2025-02-17 14:51:44 +0100

    Script update

--- Simulations/gen_deployment/gen_fix_deployment.sh
+++ Simulations/gen_deployment/gen_fix_deployment.sh
@@ -4,24 +4,28 @@
 # $2 y number of nodes
 # $3 cell size
 # $4 sim time
+# $5 x or y square
 
-export CELL_SIZE=`echo $3`
-export SIM_TIME=`echo $4`
-export FIELD_X=`echo "${3} * (${1} - 1)"|bc `
-export FIELD_Y=`echo "${3} * (${2} - 1)"|bc `
+export CELL_SIZE=`echo ${3}`
+export SIM_TIME=`echo ${4}`
+if [ ${5} = x ]; then
+    export FIELD_X=`echo "${3} * (${1} - 1)"|bc `
+    export FIELD_Y=`echo "${3} * (${1} - 1)"|bc `
+else
+    export FIELD_X=`echo "${3} * (${2} - 1)"|bc `
+    export FIELD_Y=`echo "${3} * (${2} - 1)"|bc `
+fi
 export NUM_NODES=`echo "${1} * ${2}"|bc `
-export NUM_NODES_NS=`echo "${NUM_NODES}-1"|bc`
 export NODES_X=$1
 export NODES_Y=$2
-export RANDOM_PREFIX="randomized_"
-export RND_RANGE=0.1
 
-#envsubst < deployment_fix.tmpl > deployment.ini
+envsubst < deployment_fix.tmpl > deployment.ini
 
 for y in $(seq 0 $((${NODES_Y} - 1))); do
     for x in $(seq 0 $((${NODES_X} - 1))); do
         NODE_NUM=$((${x} + ${y} * ${NODES_X}))
-        echo SN.node[${NODE_NUM}].xCoor = $((${x} * ${CELL_SIZE} ))  
+        echo SN.node[${NODE_NUM}].xCoor = $(echo "${x} * ${CELL_SIZE}"|bc ) >> deployment.ini
+        echo SN.node[${NODE_NUM}].yCoor = $(echo "${y} * ${CELL_SIZE}"|bc ) >> deployment.ini
     done
 done
 
